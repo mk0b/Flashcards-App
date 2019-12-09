f50a1003aaeb3940eeaecace7747ddcc0edc3d69
Megan Katherine O'Brien <megankob@gmail.com>
2019-12-09 15:50:58 -0500

    installed cookie parser

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+//telling my app to use pug nd set the settings.
 app.set('view engine', 'pug');
+
+//telling my app to use bodyparser url encoded and setting extended setting to false
+app.use(bodyParser.urlencoded({ extended: false}));
 
 app.listen(3000, () => {
     console.log('The application is running on localhost:3000');
@@ -14,6 +19,7 @@ app.get('/hello', (req, res) => {
 
 //need to setup a post request for our /hello form
 app.post('/hello', (req, res) => {
-    console.dir(req);
-    res.render('hello');
+    //setting the cookie up
+    res.cookie('username', req.body.username);
+    res.render('hello', { name: req.body.username});
 });
